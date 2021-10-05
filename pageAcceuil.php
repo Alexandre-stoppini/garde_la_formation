67ff244dfa6e74d2e6d7828499353a5bef278767
XTAO123456 <91958477+XTAO123456@users.noreply.github.com>
2021-10-05 16:15:12 +0200

    modifi 16h

--- pageAcceuil.php
+++ pageAcceuil.php
@@ -1,103 +1,116 @@
-<?php
-include_once "./Vue/header.php";
-?>
-<nav class="py-1">
-    <div class="container-fluid">
-        <div class="row text-center">
-            <div class="col-md-2"> <a href="#" class="h5 nav-link">Les demandes</a></div>
-            <div class="col-md-2"> <a href="#" class="h5 nav-link">Mes filleuls</a> </div>
-            <div class="col-md-4">
-                <span id="logo"></span>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?= $pageTitle ?>
+    </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../index.css">
+</head>
+
+<body>
+    <nav class="py-1">
+        <div class="container-fluid">
+            <div class="row text-center">
+                <div class="col-md-2"> <a href="#" class="h5 nav-link">Les demandes</a></div>
+                <div class="col-md-2"> <a href="#" class="h5 nav-link">Mes filleuls</a> </div>
+                <div class="col-md-4">
+                    <span id="logo"></span>
+                </div>
+                <div class="col-md-2"> <a href="#" class="h5 nav-link">Mon profil</a> </div>
+                <div class="col-md-2"> <a href="#" class="h5 nav-link" id="btn-login">Se déconnecter</a> </div>
             </div>
-            <div class="col-md-2"> <a href="#" class="h5 nav-link">Mon profil</a> </div>
-            <div class="col-md-2"> <a href="#" class="h5 nav-link" id="btn-login">Se déconnecter</a> </div>
+        </div>
+    </nav>
+    <div class="container-fluid p-0">
+        <div id="image1">
         </div>
     </div>
-</nav>
-<div class="container-fluid p-0">
-    <div id="image1">
+    <div class="container-fluid p-0">
+        <div id="image2"></div>
     </div>
-</div>
-<div class="container-fluid p-0">
-    <div id="image2"></div>
-</div>
-<div class="container-fluid py-5 px-0" id="image3">
-    <div class="row justify-content-center">
-        <div class="col-md-5 py-5 mx-3 my-3 profil-card">
-            <div class="row">
-                <div class="col-md-2 profil-img mx-2"></div>
-                <div class="col-md-9">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat officia illo quibusdam
-                        distinctio
-                        animi ipsam, ullam nostrum sapiente? Hic ex maiores harum illum iste repudiandae commodi
-                        adipisci
-                        corporis animi rerum?
-                        Voluptas dolore facere doloribus aspernatur culpa odit perspiciatis eius, architecto tempore
-                        sunt
-                        reprehenderit consectetur unde ipsum. Tempora ipsam minima aut exercitationem dicta. Quo
-                        recusandae
-                        tenetur officiis? Quis distinctio itaque unde.</p>
+    <div class="container-fluid py-5 px-0" id="image3">
+        <div class="row justify-content-center">
+            <div class="col-md-5 py-5 mx-3 my-3 profil-card">
+                <div class="row">
+                    <div class="col-md-2 profil-img mx-2"></div>
+                    <div class="col-md-9">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat officia illo quibusdam
+                            distinctio
+                            animi ipsam, ullam nostrum sapiente? Hic ex maiores harum illum iste repudiandae commodi
+                            adipisci
+                            corporis animi rerum?
+                            Voluptas dolore facere doloribus aspernatur culpa odit perspiciatis eius, architecto tempore
+                            sunt
+                            reprehenderit consectetur unde ipsum. Tempora ipsam minima aut exercitationem dicta. Quo
+                            recusandae
+                            tenetur officiis? Quis distinctio itaque unde.</p>
+                    </div>
                 </div>
-            </div>
 
-        </div>
-        <div class="col-md-5 py-5 mx-3 my-3 profil-card">
-            <div class="row">
-                <div class="col-md-2 profil-img mx-2"></div>
-                <div class="col-md-9">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat officia illo quibusdam
-                        distinctio
-                        animi ipsam, ullam nostrum sapiente? Hic ex maiores harum illum iste repudiandae commodi
-                        adipisci
-                        corporis animi rerum?
-                        Voluptas dolore facere doloribus aspernatur culpa odit perspiciatis eius, architecto tempore
-                        sunt
-                        reprehenderit consectetur unde ipsum. Tempora ipsam minima aut exercitationem dicta. Quo
-                        recusandae
-                        tenetur officiis? Quis distinctio itaque unde.</p>
+            </div>
+            <div class="col-md-5 py-5 mx-3 my-3 profil-card">
+                <div class="row">
+                    <div class="col-md-2 profil-img mx-2"></div>
+                    <div class="col-md-9">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat officia illo quibusdam
+                            distinctio
+                            animi ipsam, ullam nostrum sapiente? Hic ex maiores harum illum iste repudiandae commodi
+                            adipisci
+                            corporis animi rerum?
+                            Voluptas dolore facere doloribus aspernatur culpa odit perspiciatis eius, architecto tempore
+                            sunt
+                            reprehenderit consectetur unde ipsum. Tempora ipsam minima aut exercitationem dicta. Quo
+                            recusandae
+                            tenetur officiis? Quis distinctio itaque unde.</p>
+                    </div>
                 </div>
-            </div>
 
-        </div>
-        <div class="col-md-5 py-5 mx-3 my-3 profil-card">
-            <div class="row">
-                <div class="col-md-2 profil-img mx-2"></div>
-                <div class="col-md-9">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat officia illo quibusdam
-                        distinctio
-                        animi ipsam, ullam nostrum sapiente? Hic ex maiores harum illum iste repudiandae commodi
-                        adipisci
-                        corporis animi rerum?
-                        Voluptas dolore facere doloribus aspernatur culpa odit perspiciatis eius, architecto tempore
-                        sunt
-                        reprehenderit consectetur unde ipsum. Tempora ipsam minima aut exercitationem dicta. Quo
-                        recusandae
-                        tenetur officiis? Quis distinctio itaque unde.</p>
+            </div>
+            <div class="col-md-5 py-5 mx-3 my-3 profil-card">
+                <div class="row">
+                    <div class="col-md-2 profil-img mx-2"></div>
+                    <div class="col-md-9">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat officia illo quibusdam
+                            distinctio
+                            animi ipsam, ullam nostrum sapiente? Hic ex maiores harum illum iste repudiandae commodi
+                            adipisci
+                            corporis animi rerum?
+                            Voluptas dolore facere doloribus aspernatur culpa odit perspiciatis eius, architecto tempore
+                            sunt
+                            reprehenderit consectetur unde ipsum. Tempora ipsam minima aut exercitationem dicta. Quo
+                            recusandae
+                            tenetur officiis? Quis distinctio itaque unde.</p>
+                    </div>
                 </div>
-            </div>
 
-        </div>
-        <div class="col-md-5 py-5 mx-3 my-3 profil-card">
-            <div class="row">
-                <div class="col-md-2 profil-img mx-2"></div>
-                <div class="col-md-9">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat officia illo quibusdam
-                        distinctio
-                        animi ipsam, ullam nostrum sapiente? Hic ex maiores harum illum iste repudiandae commodi
-                        adipisci
-                        corporis animi rerum?
-                        Voluptas dolore facere doloribus aspernatur culpa odit perspiciatis eius, architecto tempore
-                        sunt
-                        reprehenderit consectetur unde ipsum. Tempora ipsam minima aut exercitationem dicta. Quo
-                        recusandae
-                        tenetur officiis? Quis distinctio itaque unde.</p>
+            </div>
+            <div class="col-md-5 py-5 mx-3 my-3 profil-card">
+                <div class="row">
+                    <div class="col-md-2 profil-img mx-2"></div>
+                    <div class="col-md-9">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat officia illo quibusdam
+                            distinctio
+                            animi ipsam, ullam nostrum sapiente? Hic ex maiores harum illum iste repudiandae commodi
+                            adipisci
+                            corporis animi rerum?
+                            Voluptas dolore facere doloribus aspernatur culpa odit perspiciatis eius, architecto tempore
+                            sunt
+                            reprehenderit consectetur unde ipsum. Tempora ipsam minima aut exercitationem dicta. Quo
+                            recusandae
+                            tenetur officiis? Quis distinctio itaque unde.</p>
+                    </div>
                 </div>
-            </div>
 
+            </div>
         </div>
     </div>
-</div>
-
-<?php
-include_once "./Vue/footer.php";
-?>
+</body>
