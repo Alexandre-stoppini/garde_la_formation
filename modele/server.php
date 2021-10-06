78bc8ddc6f2b22ac943e582a92ee8d2918547141
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-10-06 14:56:31 +0200

    ajout de la possibilité d'ajouter des demandes dans la bdd

--- modele/server.php
+++ modele/server.php
@@ -100,3 +100,16 @@ if (isset($_POST['login_user'])) {
         $stmt->close();
     }
 }
+if (isset($_POST['post_demande'])) {
+
+    $motif = mysqli_real_escape_string($db, $_POST['motif']);
+    $lieu = mysqli_real_escape_string($db, $_POST['lieu']);
+    $description = mysqli_real_escape_string($db, $_POST['description']);
+
+    if ($query = $db->prepare("INSERT INTO demande (id_user, motif, lieu, description) VALUES (" . $_SESSION['id'] . ", '$motif', '$lieu', '$description')")) {
+
+        $query->execute();
+    }
+
+
+}
