7b17a704a5b36eb4531c557028be6a1b28eafb5b
XTAO123456 <91958477+XTAO123456@users.noreply.github.com>
2021-10-06 11:24:30 +0200

    regroupement

---
+++ Vue/navbar.php
@@ -0,0 +1,14 @@
+<nav class="py-1">
+    <div class="container-fluid">
+        <div class="row text-center">
+            <div class="col-md-2"> <a href="#" class="h5 nav-link">Les demandes</a></div>
+            <div class="col-md-2"> <a href="#" class="h5 nav-link">Mes filleuls</a> </div>
+            <div class="col-md-4">
+                <span id="logo"></span>
+            </div>
+            <div class="col-md-2"> <a href="#" class="h5 nav-link">Mon profil</a> </div>
+            <div class="col-md-2"> <a href="index.php?logout='1'" class="h5 nav-link" id="btn-login">Se déconnecter</a> </div>
+
+        </div>
+    </div>
+</nav>
--- vue/page_detail.php
+++ vue/page_detail.php
@@ -1,79 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include_once "../Vue/header.php";
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page Détails</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-</head>
-
-<body>
-    <div class="container my-5">
-        <h1 class="mb-3">Détails de demande</h1>
-        <div class="row my-2">
-            <div class="col-md-4">
-                <h5>Prénom </h5>
-                <span></span>
-            </div>
-            <div class="col-md-4">
-                <h5>Nom </h5>
-                <span></span>
-            </div>
-            <div class="col-md-3">
-                <h5>Age </h5>
-                <span> 26 </span>
-            </div>
+<div class="container my-5 page_detail">
+    <h1 class="mb-3 text-center text-light">Détail de demande</h1>
+    <div class="row my-2">
+        <div class="col-md-6">
+            <h5>Prénom </h5>
+            <span>123456abc</span>
         </div>
-        <div class="row my-2">
-            <div class="col-md-6">
-                <h5>Télphone </h5>
-                <span> 06 24 52 39 56</span>
-            </div>
-            <div class="col-md-6">
-                <h5>Email </h5>
-                <span>XXX123@gmail.com</span>
-            </div>
-        </div>
-        <div class="row mt-2 mb-4">
-            <h5>Adresse </h5>
-            <span>Hangars 15 à, 19 Quai des Chartrons, 33300 Bordeaux</span>
-        </div>
-        <div class="row my-2">
-            <h5>Description</h5>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero, distinctio aliquid, at
-                perspiciatis
-                sint facilis doloribus nulla omnis error dolores quam impedit. Amet expedita doloribus fugiat
-                impedit
-                ipsa nam dignissimos.
-                Magni assumenda repellendus a cumque eaque eum perferendis quia. Quae laborum suscipit eveniet optio
-                maxime velit repellat nihil voluptate doloribus impedit, quod magnam molestias nulla. Laboriosam
-                quam
-                eum ex reiciendis!
-                Autem, minus nihil, unde, placeat quasi eligendi incidunt aliquid quo accusantium culpa perferendis
-                eius
-                omnis consectetur id. Quae nostrum recusandae repellat dolorum, obcaecati magnam ducimus ab
-                voluptates
-                saepe sint vitae.
-                Ratione earum repudiandae provident sequi qui officia optio, tempore, quos libero ab quis quas!
-                Maxime,
-                voluptatem! Dignissimos magni quos eaque vitae excepturi unde. Sed repudiandae doloremque fugit
-                laudantium labore a!
-                Cumque cum quia quae quas laboriosam ipsa praesentium maiores animi, explicabo nulla. Voluptatibus
-                ex
-                repellendus quis enim reiciendis eius quos necessitatibus, quia illum itaque quam laudantium! Dolore
-                aliquam ipsam distinctio.
-            </p>
+        <div class="col-md-6">
+            <h5>Age </h5>
+            <span> 26 </span>
         </div>
     </div>
+    <div class="row my-2">
+        <div class="col-md-6">
+            <h5>Télphone </h5>
+            <span> 06 24 52 39 56</span>
+        </div>
+        <div class="col-md-6">
+            <h5>Email </h5>
+            <span>XXX123@gmail.com</span>
+        </div>
+    </div>
+    <div class="row my-2">
+        <h5>Motif : </h5>
+        <span>besoin de parler</span>
+    </div>
+    <div class="row my-2">
+        <h5>Description</h5>
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero, distinctio aliquid, at
+            perspiciatis
+            sint facilis doloribus nulla omnis error dolores quam impedit. Amet expedita doloribus fugiat
+            impedit
+            ipsa nam dignissimos.
+            Magni assumenda repellendus a cumque eaque eum perferendis quia. Quae laborum suscipit eveniet optio
+            maxime velit repellat nihil voluptate doloribus impedit, quod magnam molestias nulla. Laboriosam
+            quam
+            eum ex reiciendis!
+            Autem, minus nihil, unde, placeat quasi eligendi incidunt aliquid quo accusantium culpa perferendis
+            eius
+            omnis consectetur id. Quae nostrum recusandae repellat dolorum, obcaecati magnam ducimus ab
+            voluptates
+            saepe sint vitae.
+            Ratione earum repudiandae provident sequi qui officia optio, tempore, quos libero ab quis quas!
+            Maxime,
+            voluptatem! Dignissimos magni quos eaque vitae excepturi unde. Sed repudiandae doloremque fugit
+            laudantium labore a!
+            Cumque cum quia quae quas laboriosam ipsa praesentium maiores animi, explicabo nulla. Voluptatibus
+            ex
+            repellendus quis enim reiciendis eius quos necessitatibus, quia illum itaque quam laudantium! Dolore
+            aliquam ipsam distinctio.
+        </p>
+    </div>
+</div>
 
 
-</body>
-
-</html>
+<?php
+include_once "../Vue/footer.php";
+?>
