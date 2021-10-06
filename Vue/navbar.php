bbf5073ddb75504476c0dd335b3692e8e41b1a86
XTAO123456 <91958477+XTAO123456@users.noreply.github.com>
2021-10-06 12:51:23 +0200

    img && css

--- Vue/navbar.php
+++ Vue/navbar.php
@@ -4,7 +4,7 @@
             <div class="col-md-2"> <a href="#" class="h5 nav-link">Les demandes</a></div>
             <div class="col-md-2"> <a href="#" class="h5 nav-link">Mes filleuls</a> </div>
             <div class="col-md-4">
-                <span id="logo"></span>
+                <a href="../index.php"><span id="logo"></span></a>
             </div>
             <div class="col-md-2"> <a href="#" class="h5 nav-link">Mon profil</a> </div>
             <div class="col-md-2"> <a href="index.php?logout='1'" class="h5 nav-link" id="btn-login">Se déconnecter</a> </div>
