305ffc783516c4fc212ed994132d8624aaa196bd
LAMBERT Mael <pydima@gmail.com>
2021-10-06 16:36:38 +0200

    inscription

--- Vue/inscription.php
+++ Vue/inscription.php
@@ -8,73 +8,88 @@ include_once "../Vue/header.php";
         <div class="py-5 text-center">
             <h2>Inscription</h2>
         </div>
-        <div class="col-md-7 col-lg-8">
+        <div class="col-md- col-lg-12 offset-2">
             <form class="needs-validation" novalidate="" method="post" action="inscription.php">
                 <div class="row g-3">
-                    <div class="col-sm-6">
+                    
+                    <div class="col-sm-4">
                         <label for="firstName" class="form-label"></label>
                         <input type="text" class="form-control" id="firstName" placeholder="Prénom" value="" required="" name="firstName">
                         <div class="invalid-feedback">
                             Ce champs est important
                         </div>
                     </div>
-
-                    <div class="col-sm-6">
-                        <label for="lastName" class="form-label">Nom</label>
-                        <input type="text" class="form-control" id="lastName" placeholder="" value="" required="" name="lastName">
+               
+                    <div class="col-sm-4 ">
+                        <label for="lastName" class="form-label"></label>
+                        <input type="text" class="form-control" id="lastName" placeholder="Nom" value="" required="" name="lastName">
                         <div class="invalid-feedback">
                             Ce champs est important
                         </div>
                     </div>
-                    <div class="col-12">
-                        <label for="email" class="form-label">Email <span class="text-muted"></span></label>
-                        <input type="email" class="form-control" id="email" name="email">
+                </div>
+                <div class="row g-3">
+                    <div class="col-4">
+                        <label for="email" class="form-label"> <span class="text-muted"></span></label>
+                        <input type="email" class="form-control" id="email" placeholder="Email" name="email">
                         <div class="invalid-feedback">
                             Veuillez entrer une adresse mail valide.
                         </div>
                     </div>
-                    <div class="col-sm-6">
-                        <label for="firstName">Mot de passe</label>
-                        <input type="password" class="form-control" id="firstName" name="psw" required>
-                        <div class="invalid-feedback">
-                            Veuillez entrer un mot de passe valide
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <label for="firstName">Confirmez votre mot de passe</label>
-                        <input type="password" class="form-control" id="lastName" name="psw-repeat" required>
-                        <div class="invalid-feedback">
-                            Veuillez entrer un mot de passe valide
-                        </div>
-                    </div>
-                    <div class="col-12">
-                        <label for="tel" class="form-label">Téléphone <span class="text-muted"></span></label>
-                        <input type="tel" class="form-control" id="tel" name="phone">
+                    <div class="col-4">
+                        <label for="tel" class="form-label"> <span class="text-muted"></span></label>
+                        <input type="tel" class="form-control" id="tel"  placeholder="Téléphone" name="phone">
                         <div class="invalid-feedback">
                             Veuillez entrer votre numéro de téléphone.
                         </div>
                     </div>
-                    <div class="col-12">
-                        <label for="age" class="form-label">Age<span class="text-muted"></span></label>
-                        <input type="age" class="form-control" id="age" name="age">
+                </div>
+                
+                <div class="row g-3">
+                    
+                <div class="col-4">
+                        <label for="age" class="form-label"><span class="text-muted"></span></label>
+                        <input type="age" class="form-control" placeholder="Age" id="age" name="age">
                         <div class="invalid-feedback">
                             Veuillez entrer votre âge
                         </div>
                     </div>
-                    <div class="col-12">
+               
+                    <div class="col-md-4">
+                        <label for="country" class="form-label"></label>
+                        <input type="text" class="form-control" placeholder="Ville" id="ville" name="address_2">
+                        <div class="invalid-feedback">
+                            Veuillez choisir une Ville
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-4">
+                        <label for="firstName"></label>
+                        <input type="password" class="form-control" id="firstName" placeholder="Mot de passe" name="psw" required>
+                        <div class="invalid-feedback">
+                            Veuillez entrer un mot de passe valide
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <label for="firstName"></label>
+                        <input type="password" class="form-control" id="lastName" placeholder="Confirmez votre mot de passe" name="psw-repeat" required>
+                        <div class="invalid-feedback">
+                            Veuillez entrer un mot de passe valide
+                        </div>
+                    </div>
+                </div>
+                   
+                   
+                    <!-- <div class="col-12">
                         <label for="address" class="form-label">Adresse</label>
                         <input type="text" class="form-control" id="address" name="address_1">
                         <div class="invalid-feedback">
                             Veuillez entrer votre adresse
                         </div>
-                    </div>
-                    <div class="col-md-5">
-                        <label for="country" class="form-label">Ville</label>
-                        <input type="text" class="form-control" id="ville" name="address_2">
-                        <div class="invalid-feedback">
-                            Veuillez choisir une Ville
-                        </div>
-                    </div>
+                    </div> -->
+                    
                     <!--                    <div class="col-md-4">-->
                     <!--                        <label for="state" class="form-label">Pays</label>-->
                     <!--                        <input type="text" class="form-control" id="country" name="country">-->
@@ -82,7 +97,9 @@ include_once "../Vue/header.php";
                     <!--                            Veuillez choisir un Pays-->
                     <!--                        </div>-->
                     <!--                    </div>-->
-                    <button class="w-100 btn btn-primary btn-lg" type="submit" name="reg_user">S'enregistrer</button>
+                    <div class="offset-2 col-4">
+                        <button class="w-100 btn btn-primary btn-lg" type="submit" name="reg_user">S'enregistrer</button>
+                    </div>          
             </form>
 
             <div class="container_signin">
