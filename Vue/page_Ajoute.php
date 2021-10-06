1dbeb2e26c4575baee1e98ffa6a06c86dbf7adc6
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-10-06 12:28:37 +0200

    ajout de l'option permettant d'ajouter des demandes

--- Vue/page_Ajoute.php
+++ Vue/page_Ajoute.php
@@ -1,3 +1,5 @@
+<?php include('../modele/server.php') ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,23 +18,23 @@
 <body>
     <div class="container my-5">
         <h1 class="text-center">Ajouter une demande</h1>
-        <form>
+        <form method="post" action="page_Ajoute.php">
             <div class="mb-3">
                 <label for="motif" class="form-label">Motif : </label>
-                <select name="motif" id="motif" class="form-select">
+                <select id="motif" class="form-select" name="motif">
                     <option value="dog">Dog</option>
                     <option value="cat">Cat</option>
                 </select>
             </div>
             <div class="mb-3">
                 <label for="lieu" class="form-label">Lieu : </label>
-                <input type="text" class="form-control" id="lieu">
+                <input type="text" class="form-control" id="lieu" name="lieu">
             </div>
             <div class="mb-3">
                 <label for="description">Description</label>
-                <textarea class="form-control" id="description"></textarea>
+                <textarea class="form-control" id="description" name="description"></textarea>
             </div>
-            <button type="submit" class="btn btn-success">Ajouter</button>
+            <button type="submit" class="btn btn-success" name="post_demande">Ajouter</button>
         </form>
     </div>
 
