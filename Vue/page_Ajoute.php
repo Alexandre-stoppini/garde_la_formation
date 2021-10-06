0bc629372deede9480074ba72ef4737f8bda749f
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-10-06 14:38:42 +0200

    ajout de l'option permettant d'ajouter des demandes

--- Vue/page_Ajoute.php
+++ Vue/page_Ajoute.php
@@ -13,28 +13,29 @@
         crossorigin="anonymous"></script>
 </head>
 
+
 <body>
-    <div class="container my-5">
-        <h1 class="text-center">Ajouter une demande</h1>
-        <form>
-            <div class="mb-3">
-                <label for="motif" class="form-label">Motif : </label>
-                <select name="motif" id="motif" class="form-select">
-                    <option value="dog">Dog</option>
-                    <option value="cat">Cat</option>
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="lieu" class="form-label">Lieu : </label>
-                <input type="text" class="form-control" id="lieu">
-            </div>
-            <div class="mb-3">
-                <label for="description">Description</label>
-                <textarea class="form-control" id="description"></textarea>
-            </div>
-            <button type="submit" class="btn btn-success">Ajouter</button>
-        </form>
-    </div>
+<div class="container my-5">
+    <h1 class="text-center">Ajouter une demande</h1>
+    <form method="post" action="page_Ajoute.php">
+        <div class="mb-3">
+            <label for="motif" class="form-label">Motif : </label>
+            <select id="motif" class="form-select" name="motif">
+                <option value="non-urgent">benevole</option>
+                <option value="urgent">aide</option>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="lieu" class="form-label">Lieu : </label>
+            <input type="text" class="form-control" id="lieu" name="lieu">
+        </div>
+        <div class="mb-3">
+            <label for="description">Description</label>
+            <textarea class="form-control" id="description" name="description"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success" name="post_demande">Ajouter</button>
+    </form>
+</div>
 
 </body>
 
