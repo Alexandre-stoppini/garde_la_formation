13fbeca7c81e8f60a03437ca708450d222fa722e
XTAO123456 <91958477+XTAO123456@users.noreply.github.com>
2021-10-06 11:47:55 +0200

    regroupe

--- vue/page_profil.php
+++ vue/page_profil.php
@@ -1,79 +1,67 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page Détails</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-</head>
-
-<body>
-    <div class="container my-5">
-        <h1 class="text-center">Connexion et sécurité</h1>
-        <div class="container mt-4">
-            <div class="row my-4">
-                <div class="col-md-9">
-                    <h5>Nom :</h5>
-                    <span>XXX XXX</span>
-                </div>
-                <div class="col-md-3">
-                    <a href="#" class="btn btn-primary">Modifier</a>
-                </div>
+<?php
+include_once "../Vue/header.php";
+?>
+<div class="container my-5">
+    <h1 class="text-center">Connexion et sécurité</h1>
+    <div class="container mt-4">
+        <div class="row my-4">
+            <div class="col-md-9">
+                <h5>Nom :</h5>
+                <span>XXX XXX</span>
             </div>
-            <div class="row my-4">
-                <div class="col-md-9">
-                    <h5>Télephone :</h5>
-                    <span>06 87 56 89 42</span>
-                </div>
-                <div class="col-md-3">
-                    <a href="#" class="btn btn-primary">Modifier</a>
-                </div>
+            <div class="col-md-3">
+                <a href="#" class="btn btn-primary">Modifier</a>
             </div>
-            <div class="row my-4">
-                <div class="col-md-9">
-                    <h5>Email :</h5>
-                    <span>XXX123@gmail.com</span>
-                </div>
-                <div class="col-md-3">
-                    <a href="#" class="btn btn-primary">Modifier</a>
-                </div>
-            </div>
-            <div class="row my-4">
-                <div class="col-md-9">
-                    <h5>Adresse :</h5>
-                    <span>XXX</span>
-                </div>
-                <div class="col-md-3">
-                    <a href="#" class="btn btn-primary">Modifier</a>
-                </div>
-            </div>
-            <div class="row my-4">
-                <div class="col-md-9">
-                    <h5>Age :</h5>
-                    <span>36</span>
-                </div>
-                <div class="col-md-3">
-                    <a href="#" class="btn btn-primary">Modifier</a>
-                </div>
-            </div>
-            <div class="row my-4">
-                <div class="col-md-9">
-                    <h5>Mot de passe :</h5>
-                    <span>**********</span>
-                </div>
-                <div class="col-md-3">
-                    <a href="#" class="btn btn-primary">Modifier</a>
-                </div>
-            </div>
-            <a href="#" class="btn btn-success">Retourner</a>
-
         </div>
-    </div>
+        <div class="row my-4">
+            <div class="col-md-9">
+                <h5>Télephone :</h5>
+                <span>06 87 56 89 42</span>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class="btn btn-primary">Modifier</a>
+            </div>
+        </div>
+        <div class="row my-4">
+            <div class="col-md-9">
+                <h5>Email :</h5>
+                <span>XXX123@gmail.com</span>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class="btn btn-primary">Modifier</a>
+            </div>
+        </div>
+        <div class="row my-4">
+            <div class="col-md-9">
+                <h5>Adresse :</h5>
+                <span>XXX</span>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class="btn btn-primary">Modifier</a>
+            </div>
+        </div>
+        <div class="row my-4">
+            <div class="col-md-9">
+                <h5>Age :</h5>
+                <span>36</span>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class="btn btn-primary">Modifier</a>
+            </div>
+        </div>
+        <div class="row my-4">
+            <div class="col-md-9">
+                <h5>Mot de passe :</h5>
+                <span>**********</span>
+            </div>
+            <div class="col-md-3">
+                <a href="#" class="btn btn-primary">Modifier</a>
+            </div>
+        </div>
+        <a href="#" class="btn btn-success">Retourner</a>
 
-</body>
+    </div>
+</div>
+<?php
+include_once "../Vue/footer.php";
+?>
