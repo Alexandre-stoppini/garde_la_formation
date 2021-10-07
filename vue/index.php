554281fd2a6525f63007678dea6e4f3ea097d871
XTAO123456 <91958477+XTAO123456@users.noreply.github.com>
2021-10-07 15:55:35 +0200

    index

--- vue/index.php
+++ vue/index.php
@@ -25,14 +25,14 @@ $offres = get_offres();
     <div id="image2" class="row ">
         <div class="col-md-3 ms-5">
             <select name="motif" class="py-3 form-select text-center option-filtre">
-                <option value="" style="display: none;">Motif :</option>
+                <option value="" style="display: none;">Trouble :</option>
                 <option class="text-dark" value="non-urgent">Envie de sortir de la solitude</option>
                 <option class="text-dark" value="urgent">Besoin de parler avec un pro</option>
             </select>
         </div>
         <div class="col-md-3">
             <select name="motif" class="py-3 form-select text-center option-filtre">
-                <option value="" style="display: none;">Motif :</option>
+                <option value="" style="display: none;">Niveau d'aide :</option>
                 <option class="text-dark" value="non-urgent">Envie de sortir de la solitude</option>
                 <option class="text-dark" value="urgent">Besoin de parler avec un pro</option>
             </select>
@@ -45,23 +45,24 @@ $offres = get_offres();
         </div>
     </div>
 </div>
+
+
 <div class="container-fluid py-5 px-0" id="image3">
     <div class="row justify-content-center">
 
-        <h2>OFFRES</h2>
+        <h2>DEMANDES</h2>
         <?php
-        foreach ($offres as $offre) {
-
+        foreach ($demandes as $demande) {
         ?>
             <div class="col-md-5 py-5 mx-3 my-3 profil-card">
                 <div class="row">
                     <div class="col-md-2 profil-img mx-2"></div>
                     <div class="col-md-9">
-                        <p><?php echo $offre['prenom'] ?></p>
-                        <p><?php echo $offre['pro'] ?></p>
-                        <p><?php echo $offre['phone'] ?></p>
-                        <p><?php echo $offre['mobilite'] ?></p>
-                        <p><?php echo $offre['description'] ?></p>
+                        <p><?php echo $demande['prenom'] ?></p>
+                        <p><?php echo $demande['motif'] ?></p>
+                        <p><?php echo $demande['phone'] ?></p>
+                        <p><?php echo $demande['lieu'] ?></p>
+                        <p><?php echo $demande['description'] ?></p>
                     </div>
                 </div>
 
@@ -73,37 +74,8 @@ $offres = get_offres();
 
     </div>
 </div>
-<?php if ($_SESSION["benevole"] == 1) { ?>
-    <hr>
-    <div class="container-fluid py-5 px-0" id="image3">
-        <div class="row justify-content-center">
-
-            <h2>DEMANDES</h2>
-            <?php
-            foreach ($demandes as $demande) {
-            ?>
-                <div class="col-md-5 py-5 mx-3 my-3 profil-card">
-                    <div class="row">
-                        <div class="col-md-2 profil-img mx-2"></div>
-                        <div class="col-md-9">
-                            <p><?php echo $demande['prenom'] ?></p>
-                            <p><?php echo $demande['motif'] ?></p>
-                            <p><?php echo $demande['phone'] ?></p>
-                            <p><?php echo $demande['lieu'] ?></p>
-                            <p><?php echo $demande['description'] ?></p>
-                        </div>
-                    </div>
-
-                </div>
-
-
-            <?php } ?>
-
-
-        </div>
-    </div>
 
 <?php
-}
+
 include_once "footer.php";
 ?>
