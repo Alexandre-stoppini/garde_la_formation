3ab6e50462fa750ebf565c5125ec0528fc7f6cd3
LAMBERT Mael <pydima@gmail.com>
2021-10-07 11:00:07 +0200

    profil

--- vue/page_profil.php
+++ vue/page_profil.php
@@ -2,63 +2,68 @@
 include_once "../Vue/header.php";
 ?>
 <div class="container my-5">
-    <h1 class="text-center">Connexion et sécurité</h1>
-    <div class="container mt-4">
-        <div class="row my-4">
-            <div class="col-md-9">
-                <h5>Nom :</h5>
-                <span>XXX XXX</span>
+
+    <div class="container">
+    <div class="detail-block"> 
+      <div class="row my-2 d-flex justify-content-center text-light ">
+            <div class="col-md-8 my-2 mx-5 text-center detail-block">
+                <h5 style="color: white;"  style="color: white;">Biographie :</h5>
+                <span style="color: white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada,
+                 risus id ultricies lobortis, justo felis consequat nisi, non iaculis tellus quam vitae lectus. Mauris at vehicula erat,
+                  eget ultricies nunc. Fusce efficitur sapien sit amet ipsum posuere dapibus. Praesent sit amet lectus aliquet, molestie sem id,
+                   commodo est. Pellentesque convallis libero a semper pretium.</span>
+            
+             </div>
+         </div>
+        <div class="row my-2 d-flex justify-content-center text-light ">
+            
+            <div class="col-md-2 my-2 mx-5 text-center detail-block">
+            <h6 style="color: white;"  style="color: white;font-size: 0.8em;">Nom :</h6>
+                <span style="color: white;">XXX XXX</span>
             </div>
-            <div class="col-md-3">
-                <a href="#" class="btn btn-primary">Modifier</a>
+         </div>
+         <div class="row my-2 d-flex justify-content-center text-light ">
+         <div class="col-md-2 my-2 mx-5 text-center detail-block">
+            <h6 style="color: white;"  style="color: white;">Prénom :</h6>
+                <span style="color: white;">XXX XXX</span>
             </div>
+         </div>
+         <div class="row my-2 d-flex justify-content-center text-light  ">
+            <div class="col-md-2 my-2 mx-5 text-center detail-block">
+            <h6 style="color: white;">Télephone :</h6>
+                <span style="color: white;">06 87 56 89 42</span>
+            </div>
+         </div>
+         <div class="row my-2 d-flex justify-content-center text-light ">
+            <div class="col-md-2 my-2 mx-5 text-center detail-block ">
+            <h6 style="color: white;">Email :</h6>
+                <span style="color: white;">XXX123@gmail.com</span>
+            </div>
+         </div>
+         <div class="row my-2 d-flex justify-content-center text-light ">
+            <div class="col-md-2 my-2 mx-5 text-center detail-block ">
+            <h6 style="color: white;">Age :</h6>
+                <span style="color: white;">36</span>
+            </div>
+         </div>
+         <div class="row my-2 d-flex justify-content-center text-light ">
+            <div class="col-md-2 my-2 mx-5 text-center detail-block ">
+            <h6 style="color: white;">Qualité :</h6>
+                <span style="color: white;">36</span>
+            </div>
+         </div>
+         <div class="row my-2 d-flex justify-content-center text-light ">
+            <div class="col-md-3 my-2 mx-5 text-center detail-block">
+            <h6 style="color: white;">Mot de passe :</h6>
+                <span style="color: white;">**********</span>
+            </div>
+         </div>
+        
+      
+            
         </div>
-        <div class="row my-4">
-            <div class="col-md-9">
-                <h5>Télephone :</h5>
-                <span>06 87 56 89 42</span>
-            </div>
-            <div class="col-md-3">
-                <a href="#" class="btn btn-primary">Modifier</a>
-            </div>
-        </div>
-        <div class="row my-4">
-            <div class="col-md-9">
-                <h5>Email :</h5>
-                <span>XXX123@gmail.com</span>
-            </div>
-            <div class="col-md-3">
-                <a href="#" class="btn btn-primary">Modifier</a>
-            </div>
-        </div>
-        <div class="row my-4">
-            <div class="col-md-9">
-                <h5>Adresse :</h5>
-                <span>XXX</span>
-            </div>
-            <div class="col-md-3">
-                <a href="#" class="btn btn-primary">Modifier</a>
-            </div>
-        </div>
-        <div class="row my-4">
-            <div class="col-md-9">
-                <h5>Age :</h5>
-                <span>36</span>
-            </div>
-            <div class="col-md-3">
-                <a href="#" class="btn btn-primary">Modifier</a>
-            </div>
-        </div>
-        <div class="row my-4">
-            <div class="col-md-9">
-                <h5>Mot de passe :</h5>
-                <span>**********</span>
-            </div>
-            <div class="col-md-3">
-                <a href="#" class="btn btn-primary">Modifier</a>
-            </div>
-        </div>
-        <a href="#" class="btn btn-success">Retourner</a>
+        <br>
+        <a href="#" class="h5 nav-link" id="btn-profil" >Retourner</a>
 
     </div>
 </div>
