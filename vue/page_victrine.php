677ffa7dad0371cf80aa084a9dcaa892ab68344e
XTAO123456 <91958477+XTAO123456@users.noreply.github.com>
2021-10-07 11:51:42 +0200

    landing

--- vue/page_victrine.php
+++ vue/page_victrine.php
@@ -25,17 +25,17 @@
     <div class="row">
         <div class="col-md-3 text-center"><img src="./src/img/logo.png" alt="" style="width: 100px; height:100px; border-radius:50% "></div>
         <div class="col-md-9 ">
-            <p class="text-light h3 me-5">
+            <p class="text-light h3 me-5" style="text-shadow: grey 2px 3px;">
                 Rejoins la plate-forme d'aide à la formation et rencontre des professionnels pour t'aider à réussir.
             </p>
-            <p class="text-light h3 me-5">
+            <p class="text-light h3 me-5" style="text-shadow: grey 2px 3px;">
                 Si vous voulez accompagner et contribuer à la réussite de tous, inscrivez vous.
             </p>
         </div>
     </div>
     <div class="row justify-content-center my-5">
-        <div class="col-md-7 text-center">
-            <input type="text" class="form-control py-3 px-5" id="recherche" placeholder="Entrer votre adrress mail" style="width: 80%; display:inline-block;
+        <div class="col-md-8" style="position: relative;left:100px;">
+            <input type="text" class="form-control py-3 px-5" id="recherche" placeholder="Entrer votre adrresse mail" style="width: 80%; display:inline-block;
             border-radius: 30px; font-size:20px;">
             <a href="#" class="btn text-dark px-3 py-1" style="z-index: 999;
         position: relative;
@@ -48,7 +48,7 @@
 
     </div>
     <div class="row">
-        <div class="col-md-6 text-center"><img src="./src/img/Les_annonces1.png" style="width: 700px;" alt="les_annonces"></div>
-        <div class="col-md-6 text-center"><img src="./src/img/Mon_profil_1.png" style="width: 700px;" alt="mon_profil"></div>
+        <div class="col-md-6 text-center"><img src="./src/img/Les_annonces1.png" style="width: 85%; height:95%" alt="les_annonces"></div>
+        <div class="col-md-6 text-center"><img src="./src/img/Mon_profil_1.png" style="width: 85%; height:95%" alt="mon_profil"></div>
     </div>
 </div>
