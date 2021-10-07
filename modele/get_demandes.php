563016323c4fe624c5c3e3d133053d3044fa45be
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-10-07 15:24:48 +0200

    fixe get demandes

--- modele/get_demandes.php
+++ modele/get_demandes.php
@@ -2,7 +2,7 @@
 function get_demande()
 {
     $db = mysqli_connect('localhost', 'root', '', 'garde_la_formation');
-    $demandes = mysqli_query($db, "SELECT demande.motif, demande.description, demande.lieu, users.prenom, users.phone FROM demande INNER JOIN users ON users.id = demande.id_user");
+    $demandes = mysqli_query($db, "SELECT demande.motif, demande.description, demande.lieu, users.prenom, users.phone, users.disease FROM demande INNER JOIN users ON users.id = demande.id_user");
     $ar_inter = [];
     $ar_result = [];
     while ($row = mysqli_fetch_array($demandes)) {
@@ -11,9 +11,31 @@ function get_demande()
     foreach ($ar_inter as $value) {
 
 
-        for ($i = 0; $i<5; $i++){
+        for ($i = 0; $i < 5; $i++) {
             unset ($value[$i]);
         }
+        $ar_result[] = $value;
+    }
+    return ($ar_result);
+}
+
+function get_offres(){
+    $db = mysqli_connect('localhost', 'root', '', 'garde_la_formation');
+    $offres = mysqli_query($db, "SELECT offre.mobilite, offre.description, users.prenom, users.phone, users.pro FROM offre INNER JOIN users ON users.id = offre.id_user");
+    $ar_inter = [];
+    $ar_result = [];
+    while ($row = mysqli_fetch_array($offres)) {
+        $ar_inter[] = $row;
+    }
+    foreach ($ar_inter as $value) {
+
+
+        for ($i = 0; $i < 5; $i++) {
+            unset ($value[$i]);
+        }
+
+        $value["pro"] = $value["pro"] == "1" ? "Professionnel de la santé" : "Bénévole sans diplôme";
+
         $ar_result[] = $value;
     }
     return ($ar_result);
